05c11bc6966e1a4d6e7fe8e50b2a7547e1959245
Nosa Agho <nosaoasis@gmail.com>
2023-04-29 12:44:09 +0100

    Refacored
    Refactored the code to implement the wordsearch and also pass the test cases.

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
+// from the previous PP activity
+const transposed = (matrix) => {
+  const newMatrix = [];
 
-module.exports = wordSearch
+  for (let i = 0; i < matrix[0].length; i++) {
+    newMatrix.push([]);
+    for (let j = 0; j < matrix.length; j++) {
+      newMatrix[i][j] = matrix[j][i];
+    }
+  }
+
+  return newMatrix;
+};
+
+const wordSearch = (letters, word) => {
+  const horizontalJoin = letters.map((ls) => ls.join(""));
+  for (l of horizontalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  const verticalJoin = transposed(letters).map((ls) => ls.join(""));
+  for (l of verticalJoin) {
+    if (l.includes(word)) return true;
+  }
+
+  return false;
+};
+
+module.exports = wordSearch;
